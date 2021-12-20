90e99f6d376fd912112abcccb06a55dfaad38c6a
barmoshe <1barmoshe1@gmail.com>
2021-12-20 11:23:16 +0200

    q1 data structs  3

---
+++ Q1/Q1/list_Q1.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef __LIST__
+#define __LIST__
+typedef struct node
+{
+	int num;
+	struct node *next;
+} Node;
+int sumListAndFindTail(Node *head, Node *tail);
+int biggerSumList(Node *head1, Node *head2, Node *tail1, Node *tail2);
+Node *combine_lists(Node *head1, Node *head2);
+#endif
---
+++ Q1/Q1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list_Q1.h"
+
+int main()
+{
+	return 1;
+}
