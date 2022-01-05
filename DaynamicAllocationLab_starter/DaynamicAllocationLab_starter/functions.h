714529fa02a2fb4e695e8a0813fff90813f3460c
barmoshe <1barmoshe1@gmail.com>
2022-01-05 20:52:58 +0200

    lab 3 complete

--- DaynamicAllocationLab_starter/DaynamicAllocationLab_starter/functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//getEvenNumbers
-//getEvenNumbers_OnePass
-//createCombineStrings
-
-
-
--- DaynamicAllocationLab_starter/DaynamicAllocationLab_starter/generalStrings.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define MAX_LENGTH 255
-
-char*	getStrExactLength();  
-char* 	myGets(char* buf, int size);
